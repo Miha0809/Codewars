8468c7ba0afb963e185c2e85aab2359667a3a168
Miha0809 <miha080104@gmail.com>
2024-12-05 23:00:31 +0100

    Init

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Codewars;
+
+partial class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(FindMissingLetter(new [] { 'a','b','c','d','f' }));
+    }
+
+    public static char FindMissingLetter(char[] array)
+    {
+        for (int i = 1; i < array.Length; i++)
+        {
+            var prevChar = ((int)array[i-1]) + 1;
+            var currentChar = (int)array[i];
+
+            if (prevChar != currentChar)
+            {
+                return (char)prevChar;
+            }
+        }
+
+        return ' ';
+    }
+}
