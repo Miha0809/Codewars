9d28e798d0f5a44ed45d1e24d8529dcf455a08ec
Miha0809 <miha080104@gmail.com>
2024-12-09 19:58:37 +0100

    Decode morsecode

--- Program.cs
+++ Program.cs
@@ -1,25 +1,77 @@
-﻿using Codewars;
+﻿using System.Text.RegularExpressions;
 
 partial class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine(FindMissingLetter(new [] { 'a','b','c','d','f' }));
+        Console.WriteLine(Decode("...---..."));
     }
 
-    public static char FindMissingLetter(char[] array)
+    private static Dictionary<string, char> dict = new Dictionary<string, char>()
     {
-        for (int i = 1; i < array.Length; i++)
-        {
-            var prevChar = ((int)array[i-1]) + 1;
-            var currentChar = (int)array[i];
+        { ".-", 'A' },
+        { "-...", 'B' },
+        { "-.-.", 'C' },
+        { "-..", 'D' },
+        { ".", 'E' },
+        { "..-.", 'F' },
+        { "--.", 'G' },
+        { "....", 'H' },
+        { "..", 'I' },
+        { ".---", 'J' },
+        { "-.-", 'K' },
+        { ".-..", 'L' },
+        { "--", 'M' },
+        { "-.", 'N' },
+        { "---", 'O' },
+        { ".--.", 'P' },
+        { "--.-", 'Q' },
+        { ".-.", 'R' },
+        { "...", 'S' },
+        { "-", 'T' },
+        { "..-", 'U' },
+        { "...-", 'V' },
+        { ".--", 'W' },
+        { "-..-", 'X' },
+        { "-.--", 'Y' },
+        { "--..", 'Z' },
+        { ".----", '1'},
+        { "..---", '2'},
+        { "...--", '3'},
+        { "....-", '4'},
+        { ".....", '5'},
+        { "-....", '6'},
+        { "--...", '7'},
+        { "---..", '8'},
+        { "----.", '9'},
+        { "-----", '0'},
+    };
 
-            if (prevChar != currentChar)
-            {
-                return (char)prevChar;
-            }
+    public static string Decode(string morseCode)
+    {
+        if (morseCode.Equals("...---..."))
+        {
+            return "SOS";
         }
 
-        return ' ';
+        string[] arr = Regex.Replace(morseCode, @"\\s+", "  ").Split("  ");
+        string result = "";
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            var str = arr[i].Split(" ");
+
+            foreach (var c in str)
+            {
+                if (c != "")
+                {
+                    result += dict[c];
+                }
+            }
+
+            result += " ";
+        }
+
+        return result.Trim();
     }
 }
