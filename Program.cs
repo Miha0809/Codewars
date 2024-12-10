8a70d348682cc23609ddced1286e40e50e810bcd
Miha0809 <miha080104@gmail.com>
2024-12-10 19:15:24 +0100

    Friend or Foe?

--- Program.cs
+++ Program.cs
@@ -2,24 +2,24 @@
 {
     public static void Main(string[] args)
     {
-        foreach(var item in GetIntegersFromList(new List<object>(){1, 2, "a", "b", "aasf", "1", "123", 123}))
+        foreach (var item in FriendOrFoe(["Mark", "Jimmy", "Abel", "Amanda"]))
         {
             Console.WriteLine(item);
         }
     }
 
-    public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
-   {
-      List<int> result = new List<int>();
-     
-      foreach (var item in listOfItems)
-      {
-          if (item.GetType() == typeof(int))
-          {
-              result.Add(Convert.ToInt32(item));
-          }
-      }
-     
-     return result;
-   }
+    public static IEnumerable<string> FriendOrFoe(string[] names)
+    {
+        List<string> friends = new List<string>();
+
+        foreach (var name in names)
+        {
+            if (name.Length == 4)
+            {
+                friends.Add(name);
+            }
+        }
+
+        return friends;
+    }
 }
