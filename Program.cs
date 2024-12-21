b71076428009a1ea5c07f25876e134af453bdf85
Miha0809 <miha080104@gmail.com>
2024-12-21 21:46:41 +0100

    Bit Counting

--- Program.cs
+++ Program.cs
@@ -1,24 +1,16 @@
-﻿partial class Program
+﻿using System.Text.RegularExpressions;
+
+partial class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine(DigitalRoot(16));
+        Console.WriteLine(CountBits(1234));
     }
 
-    public static int DigitalRoot(long n, long suma = 0)
+    public static int CountBits(int n)
     {
-        if (n > 9)
-        {
-            while (n > 0)
-            {
-                var lastNumber = n % 10;
-                suma += lastNumber;
-                n /= 10;
-            }
-
-            return DigitalRoot(suma);
-        }
-
-        return (int)n;
+        string binary = Convert.ToString(n, 2);
+        var countPositives = Regex.Matches(binary, "1").Count;
+        return countPositives;
     }
 }
